bb6944076f3d4e09b3e48a65c70d163d03ab9658
guhj <guhj-a@grandsoft.com.cn>
2014-06-06 10:33:44 +0800

    Add a Thread test case

---
+++ test-for-study/src/main/java/com/guhanjie/jdk/thread/TestThread.java
@@ -0,0 +1,10 @@
+package com.guhanjie.jdk.thread;
+
+public class TestThread {
+
+	public static void main(String[] args) {
+		System.out.println(Thread.currentThread());
+		System.out.println(Thread.activeCount());
+	}
+
+}
