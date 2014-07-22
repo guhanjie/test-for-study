2f357cf00038022cfbb4691990520b25455db2f1
guhj <guhj-a@grandsoft.com.cn>
2014-07-22 16:09:08 +0800

    Add a test example for JAVA Thread

--- test-for-study/src/main/java/com/guhanjie/jdk/thread/TestThread.java
+++ test-for-study/src/main/java/com/guhanjie/jdk/thread/TestThread.java
@@ -5,6 +5,41 @@ public class TestThread {
 	public static void main(String[] args) {
 		System.out.println(Thread.currentThread());
 		System.out.println(Thread.activeCount());
+		
+		Data data = new Data(0);
+		TThread t1 = new TThread(data);
+		TThread t2 = new TThread(data);
+		TThread t3 = new TThread(data);
+		t1.start();
+		t2.start();
+		t3.start();
 	}
 
+}
+
+class Data {
+	private int i;		//volatile
+	public Data(int i) {
+		this.i = i;
+	}
+	public void setData(int i) {
+		this.i = i;
+	}
+	public int getData() {
+		return i;
+	}
+}
+
+class TThread extends Thread {
+	public Data data;
+	public TThread(Data data) {
+		this.data = data;
+	}
+	public void run() {
+		for(int i=0; i<3; i++) {
+			int nn = data.getData();
+			System.out.println("thread:"+Thread.currentThread().getName()+" data = "+nn);
+			data.setData(++nn);
+		}
+	}
 }
